9313ce7dcae5e5333183d0db26e82c07c01d0706
yanshihao <504982352@qq.com>
2020-11-06 15:23:36 +0800

    修改

--- app/src/main/java/com/zq/transition/shareelementtransition/browse/RecyclerCardAdapter.java
+++ app/src/main/java/com/zq/transition/shareelementtransition/browse/RecyclerCardAdapter.java
@@ -8,58 +8,59 @@ import android.widget.ImageView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 import com.zq.transition.R;
 
 public class RecyclerCardAdapter extends RecyclerView.Adapter<RecyclerCardAdapter.CardHolder> {
 
-	private Context             mContext;
-	private OnItemClickListener mListener;
+    private Context mContext;
+    private OnItemClickListener mListener;
 
-	public interface OnItemClickListener {
+    public interface OnItemClickListener {
 
-		void onItemClick(View view, int position);
-	}
+        void onItemClick(View view, int position);
+    }
 
-	public RecyclerCardAdapter(Context context, OnItemClickListener listener) {
-		mContext = context;
-		mListener = listener;
-	}
+    public RecyclerCardAdapter(Context context, OnItemClickListener listener) {
+        mContext = context;
+        mListener = listener;
+    }
 
-	@Override
-	public CardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		final CardHolder holder = new CardHolder(LayoutInflater.from(mContext).inflate(R.layout.item_card_album_image, parent, false));
-		holder.mAlbumImage.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				mListener.onItemClick(holder.mAlbumImage, holder.getAdapterPosition());
-			}
-		});
-		return holder;
-	}
+    @Override
+    public CardHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final CardHolder holder = new CardHolder(LayoutInflater.from(mContext).inflate(R.layout.item_card_album_image, parent, false));
+        holder.mAlbumImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onItemClick(holder.mAlbumImage, holder.getAdapterPosition());
+            }
+        });
+        return holder;
+    }
 
-	@Override
-	public void onBindViewHolder(CardHolder holder, int position) {
-		Picasso.with(mContext).load(ImageConstants.IMAGE_SOURCE[position]).into(holder.mAlbumImage);
-		/**
-		 * 设置共享元素的名称
-		 */
-		holder.mAlbumImage.setTransitionName(ImageConstants.IMAGE_SOURCE[position]);
-		holder.mAlbumImage.setTag(ImageConstants.IMAGE_SOURCE[position]);
-	}
+    @Override
+    public void onBindViewHolder(CardHolder holder, int position) {
+        Glide.with(mContext).load(ImageConstants.IMAGE_SOURCE[position]).into(holder.mAlbumImage);
+        /**
+         * 设置共享元素的名称
+         */
+        holder.mAlbumImage.setTransitionName(ImageConstants.IMAGE_SOURCE[position]);
+        holder.mAlbumImage.setTag(ImageConstants.IMAGE_SOURCE[position]);
+    }
 
-	@Override
-	public int getItemCount() {
-		return ImageConstants.IMAGE_SOURCE.length;
-	}
+    @Override
+    public int getItemCount() {
+        return ImageConstants.IMAGE_SOURCE.length;
+    }
 
-	static class CardHolder extends RecyclerView.ViewHolder {
+    static class CardHolder extends RecyclerView.ViewHolder {
 
-		final ImageView mAlbumImage;
+        final ImageView mAlbumImage;
 
-		CardHolder(View itemView) {
-			super(itemView);
-			mAlbumImage = itemView.findViewById(R.id.main_card_album_image);
-		}
-	}
+        CardHolder(View itemView) {
+            super(itemView);
+            mAlbumImage = itemView.findViewById(R.id.main_card_album_image);
+        }
+    }
 }
